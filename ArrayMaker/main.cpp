5361d5ae409ad6bff07314ba7b3f8456c9701b00
Maria del Mar <maria.arbelaezs@udea.edu.co>
2022-02-20 19:55:41 -0500

    Agrego programa de creacion de arrays

---
+++ ArrayMaker/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+        //Read file
+        return 0;
+
+    }
+    /* //Creación de array de 8 bits de manera random
+    int main(){
+        srand (time(NULL)); //Semilla de Random
+        //Generar aleatoriamente
+        int num;
+        num=rand() % 100;
+        unsigned char* array=new unsigned char[100];
+        for (int i=0;i<num;i++){
+            *(array+i)=rand() % 256;
+        }
+    }*/
